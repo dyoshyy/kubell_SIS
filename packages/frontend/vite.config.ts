709b6637358aa6becb229263cf128963d7b561d0
SUDA Mikihiro <mikihiro.suda@chatwork.com>
2024-09-03 04:17:21 +0000

    chore: vitestのwatchモードを無効化

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
+    watch: false,
   },
   resolve: {
     alias: {
