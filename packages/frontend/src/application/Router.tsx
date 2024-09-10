028903e4320b10983245e3cb593c057b2f1fb5ef
dyoshy <d.yoshhyy@gmail.com>
2024-09-10 13:13:12 +0000

    refac

--- packages/frontend/src/application/Router.tsx
+++ packages/frontend/src/application/Router.tsx
@@ -42,36 +42,6 @@ const StyledMain = styled.main`
   display: flex;
 `;
 
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const Toolbar = styled.div`
-  display: flex;
-  background: #cccccc;
-  justify-content: flex-end;
-  gap: 10px;
-  padding: 10px;
-  width: 100%;
-  height: 50px;
-`;
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const Toolbar = styled.div`
-  display: flex;
-  background: #cccccc;
-  justify-content: flex-end;
-  gap: 10px;
-  padding: 10px;
-  width: 100%;
-  height: 50px;
-`;
-
 const GroupChatControls = styled.div`
   width: 20%;
   padding: ${gutterBy(1)};
