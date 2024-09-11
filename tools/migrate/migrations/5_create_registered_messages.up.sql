e8ed24391b20d35df68f9cd68d5005f3ca0b7492
dyoshy <d.yoshhyy@gmail.com>
2024-09-11 13:56:58 +0000

    feat: generate table

---
+++ tools/migrate/migrations/5_create_registered_messages.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `registered_messages`
+(
+    `id`         varchar(64) NOT NULL,
+    `disabled`   tinyint(1)  NOT NULL,
+    `title`       varchar(64) NOT NULL,
+    `body`       varchar(64) NOT NULL,
+    `created_at` datetime    NOT NULL,
+    `updated_at` datetime    NOT NULL,
+    PRIMARY KEY (`id`),
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
