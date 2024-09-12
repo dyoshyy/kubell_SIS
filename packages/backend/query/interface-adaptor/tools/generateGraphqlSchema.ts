64cd8fe0e766a9e239709ab47ad1794f290c40a1
Kunorin <99805829+KotaKunori@users.noreply.github.com>
2024-09-12 02:43:47 +0000

    read apiを実装．

--- packages/backend/query/interface-adaptor/tools/generateGraphqlSchema.ts
+++ packages/backend/query/interface-adaptor/tools/generateGraphqlSchema.ts
@@ -1,10 +1,10 @@
 import path from "path";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
-import { GroupChatQueryResolver, ProjectQueryResolver } from "../src/graphql/resolvers";
+import { GroupChatQueryResolver, ProjectQueryResolver, RegisteredMessageQueryResolver } from "../src/graphql/resolvers";
 
 void buildSchema({
-  resolvers: [GroupChatQueryResolver, ProjectQueryResolver],
+  resolvers: [GroupChatQueryResolver, ProjectQueryResolver, RegisteredMessageQueryResolver],
   emitSchemaFile: path.resolve(
     __dirname,
     "../../../../../schema/query.graphql",
